e89f22754f10711bac903bad8b5c7abb1fbc7032
crosgar <cecilia.rossi.garcia@gmail.com>
2021-05-09 18:29:30 -0700

    added link to first project tile

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$("#dinner-modal").on("shown.bs.modal", function () {
-  $(".modal-dialog").trigger("focus");
-});
-
-$('#proj-card').on('click', function(){
-   $("#dinner-modal").modal(options);
-});
